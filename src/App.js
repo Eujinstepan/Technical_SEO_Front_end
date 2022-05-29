03e3c7ffe2c10cde14b8541ca23bb5b7bd1d857b
jiraya001207 <jiraya001207@gmail.com>
2022-05-30 08:26:52 +0900

    update design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React, { useState } from "react";
 import JSONPretty from "react-json-pretty";
 import "./App.css";
 
+const ResultContainer = ({ label, buttonText, isObject, data }) => {
+  const [showResult, setShowResult] = useState(false);
+
+  return (
+    <>
+      <div>
+        <div className="result-container-style">
+          <span>{label}</span>
+          <button className="btn-style" onClick={() => setShowResult(!showResult)}>
+            {buttonText}
+          </button>
+        </div>
+        <div
+          style={{
+            display: showResult ? "block" : "none",
+          }}
+        >
+          <pre className="result-rendering-style" >
+            <textarea style={{ display: "flex", justifyContent: "center", width: "100%", height: "200px", fontSize: "18px", fontWeight: "300" }}>
+            {isObject ? JSON.stringify(data, null, 2) : data}
+            </textarea>
+          </pre>
+        </div>
+      </div>
+    </>
+  );
+};
+
 const App = () => {
   const [url, setUrl] = useState("");
   const [data, setData] = useState(null);
@@ -13,34 +41,54 @@ const App = () => {
   };
 
   return (
-    <div className="App" style={{ width: "900px" }}>
-      <input value={url} onChange={(e) => setUrl(e.target.value)} />
+    <div className="App">
+      <input
+        className="input-style"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
       <button onClick={handleSubmit}>Submit</button>
 
       <div style={{}}>
-        <div style={{ fontSize: "36px" }}>Result:</div>
+        <div className="result-font">Result</div>
         {data && (
           <>
-            <div>
-              <span style={{ fontSize: "26px" }}>pageSpeed: </span>
-              <pre>{JSON.stringify(data.pagespeed, null, 2)}</pre>
-            </div>
-            <div>
-              <span style={{ fontSize: "26px" }}>Leverage "Inspect URL" feature in GSC: </span>
-              <pre>{data.leverage}</pre>
-            </div>
-            <div>
-              <span style={{ fontSize: "26px" }}>Check Canonical Tags: </span>
-              <pre>{JSON.stringify(data.canonical, null, 2)}</pre>
-            </div>
-            <div>
-              <span style={{ fontSize: "26px" }}>Make sure you are using HTTPS: </span>
-              <pre>{JSON.stringify(data.Ishttpsurl, null, 2)}</pre>
-            </div>
-            <div>
-              <span style={{ fontSize: "26px" }}>Add structured data: </span>
-              <pre>{JSON.stringify(data.StructureData, null, 2)}</pre>
-            </div>
+            <ResultContainer
+              label="Check your site's loading speed"
+              buttonText="show result"
+              isObject={true}
+              data={data.pagespeed}
+            />
+            <ResultContainer
+              label="Leverage 'Inspect URL' feature in GSC "
+              buttonText="show result"
+              isObject={true}
+              data={data.leverage}
+            />
+            <ResultContainer
+              label="Ensure your website is mobile-friendly "
+              buttonText="show result"
+              isObject={true}
+              data={data.isMobileFriendly}
+            />
+            <ResultContainer
+              label="Check Canonical Tags "
+              buttonText="show result"
+              isObject={true}
+              data={data.canonical}
+            />
+            <ResultContainer
+              label="Make sure you are using HTTPS"
+              buttonText="show result"
+              isObject={true}
+              data={data.Ishttpsurl}
+            />
+            <ResultContainer
+              label="Add structured data"
+              buttonText="show result"
+              isObject={true}
+              data={data.StructureData}
+            />
           </>
         )}
       </div>
